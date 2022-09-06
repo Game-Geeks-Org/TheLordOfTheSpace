2ddd01d073f4ad53827f50669bc6abe705b93936
Ayush <aayush.sang@gmail.com>
2022-09-06 23:30:10 +0530

    Add game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Fragment, useCallback, useEffect, useState } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
-function App() {
+const App = () => {
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const {
+    unityProvider,
+    isLoaded,
+    unload,
+    loadingProgression,
+    addEventListener,
+    removeEventListener,
+    sendMessage
+  } = useUnityContext({
+    loaderUrl: "SSBuild/Build/webgl.loader.js",
+    dataUrl: "SSBuild/Build/webgl.data",
+    frameworkUrl: "SSBuild/Build/webgl.framework.js",
+    codeUrl: "SSBuild/Build/webgl.wasm",
+  });
+
+  const handleGameOver = useCallback((score) => {
+    setIsGameOver(true);
+    setScore(score);
+    console.log(` You've scored ${score} points.`)
+  }, []);
+
+  useEffect(() => {
+    addEventListener("GameOver", handleGameOver);
+    return () => {
+      removeEventListener("GameOver", handleGameOver);
+    };
+  }, [addEventListener, removeEventListener, handleGameOver]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Unity
+        className="unity"
+        unityProvider={unityProvider}
+        style={{
+          width: "100%",
+          aspectRatio: "16:9",
+          overflow: "hidden",
+        }}
+      />
+      {isGameOver === true && (
+        <p style={{ color: "white" }}>{`Game Over You've scored ${score} points.`}</p>
+      )}
+    </Fragment>
   );
 }
 
